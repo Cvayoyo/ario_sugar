ef771c112ec7c46bbd5139605a9eb6d2b89ae2dc
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-15 14:46:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 nproc=$(nproc --all)
-sudo apt-get update -y
-sudo apt-get install wget -y
-sudo apt-get install unzip -y
-sudo apt-get install git -y
-sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake -y
 sudo apt-get install git screen
-sudo apt install libsodium-dev -y
-sudo unzip sugar.zip
-sudo cd sugar
 chmod +x *
-chmod +x * /sugar
 sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
@@ -21,4 +12,4 @@ rm mycron
 sudo systemctl enable cron.service
 update-rc.d cron defaults
 sudo chmod +x *
-screen -d -m bash -c "cd sugar ; chmod +x * ; ./autogen.sh ; ./configure CFLAGS='-O3' ; make ; ./mine.sh" &
+screen -d -m bash -c "cd ario_sugar ; ./mine.sh" &
