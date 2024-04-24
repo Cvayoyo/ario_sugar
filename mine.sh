973e7645f7b020faf041732d4b7ba6f38584804b
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-24 17:08:03 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-sudo ./cpuminer-sse2 -a yespowerr16 -o stratum+tcps://stratum-na.rplant.xyz:13382 -u YVfJQT57U2iStroXVZF52FzYtEfgdSCduP --cpu-affinity $nproc -p start=0.1,d=0.1
+sudo ./cpuminer-sse2 -a yespowerr16 -o stratum+tcps://stratum-na.rplant.xyz:13382 -u YVfJQT57U2iStroXVZF52FzYtEfgdSCduP --cpu-affinity $nproc -p start=1,d=1
