bf5e18569525a4b99c3c1a07a659aade61d658a0
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-24 17:49:07 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-sudo ./cpuminer-sse2 -a yespowersugar -o stratum+tcp://nomp.mofumofu.me:3392 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4 --cpu-affinity $nproc -p start=0.1,d=0.1
+sudo ./cpuminer-sse2 -a yespowersugar --url stratum+tcp://yespowerSUGAR.na.mine.zpool.ca:6241 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4 --cpu-affinity $nproc -p start=0.1,d=0.1,c=SUGAR
